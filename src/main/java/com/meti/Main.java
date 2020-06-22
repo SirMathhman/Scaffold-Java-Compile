db25be3e9efb8b5bdf955f06d014e0912d879a39
SirMathhman <sirmathhman@gmail.com>
2020-06-21 21:36:07 -0400

    Clean Main with better logging

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -1,62 +1,144 @@
 package com.meti;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-public class Main {
+public final class Main {
 	public static final Path COMPILE = Paths.get(".", "target", "compile");
 	public static final Path SOURCE = Paths.get(".", "src", "main", "java");
+	public static final Logger logger = Logger.getLogger("Scaffold");
 
-	public static void main(String[] args) {
-		new Main().run();
+	private Main() {
 	}
 
-	private void run() {
-		try {
-			List<String> command = buildCommand();
-			execute(command);
+	public static void main(String[] args) {
+		run();
+	}
 
-			Files.walk(SOURCE)
-					.filter(path -> path.toString().endsWith(".class"))
-					.forEach(this::move);
+	private static void run() {
+		runCompilation();
+		cleanup();
+		moveClasses();
+	}
+
+	private static void runCompilation() {
+		try {
+			execute(buildCommand());
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.log(Level.SEVERE, "Failed to run compilation command.", e);
 		}
 	}
 
-	private void execute(List<String> command) throws IOException {
+	private static void cleanup() {
+		try {
+			Files.walkFileTree(COMPILE, new DeleteFileVisitor());
+		} catch (IOException e) {
+			logger.log(Level.WARNING, "Failed to cleanup class files.", e);
+		}
+	}
+
+	private static void moveClasses() {
+		try {
+			moveClassesExceptionally();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Failed to move classes.", e);
+		}
+	}
+
+	private static void execute(Collection<String> command) throws IOException {
 		Process builder = new ProcessBuilder(command.toArray(String[]::new))
 				.directory(SOURCE.toFile())
 				.start();
-		builder.getInputStream().transferTo(System.out);
-		builder.getErrorStream().transferTo(System.err);
+		String outputString = readStreamAsString(builder.getInputStream());
+		String errorString = readStreamAsString(builder.getErrorStream());
+		if (errorString.isBlank()) {
+			if (outputString.isBlank()) {
+				logger.log(Level.INFO, "Command produced no output.");
+			} else {
+				logger.log(Level.INFO, MessageFormat.format("Command produced output:{0}\t{1}",
+						System.lineSeparator(), outputString));
+			}
+		} else {
+			logger.log(Level.SEVERE, String.format("Failed to execute %s", command));
+		}
 	}
 
-	private List<String> buildCommand() throws IOException {
+	private static List<String> buildCommand() throws IOException {
 		List<String> command = new ArrayList<>();
 		command.add("javac");
-		Files.walk(SOURCE)
-				.filter(path -> path.toString().endsWith(".java"))
-				.map(SOURCE::relativize)
-				.map(Path::toString)
-				.forEach(command::add);
+		command.addAll(collectJavaPaths());
 		return command;
 	}
 
-	private void move(Path path) {
+	private static void moveClassesExceptionally() throws IOException {
+		Files.walk(SOURCE)
+				.filter(path -> path.toString().endsWith(".class"))
+				.forEach(Main::move);
+	}
+
+	private static String readStreamAsString(InputStream inputStream) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		inputStream.transferTo(outputStream);
+		return outputStream.toString();
+	}
+
+	private static List<String> collectJavaPaths() throws IOException {
+		return Files.walk(SOURCE)
+				.filter(path -> path.toString().endsWith(".java"))
+				.map(SOURCE::relativize)
+				.map(Path::toString)
+				.collect(Collectors.toList());
+	}
+
+	private static void move(Path path) {
 		Path in = SOURCE.relativize(path);
 		Path out = COMPILE.resolve(in);
+		ensureParent(out);
+		move(path, out);
+	}
+
+	private static void ensureParent(Path path) {
 		try {
-			Path parent = out.getParent();
-			if (!Files.exists(parent)) Files.createDirectories(parent);
-			Files.deleteIfExists(out);
-			Files.move(path, out);
+			Path parent = path.getParent();
+			if (!Files.exists(parent)) {
+				Files.createDirectories(parent);
+				logger.log(Level.FINER, String.format("Created parent directory %s", parent));
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.log(Level.WARNING, String.format("Failed to delete parent directory %s", path), e);
+		}
+	}
+
+	private static void move(Path from, Path to) {
+		try {
+			Files.deleteIfExists(to);
+			Files.move(from, to);
+		} catch (IOException e) {
+			logger.log(Level.WARNING, MessageFormat.format("Failed to move path from {0} to {1}", from, to), e);
+		}
+	}
+
+	private static class DeleteFileVisitor extends SimpleFileVisitor<Path> {
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			Files.delete(file);
+			return FileVisitResult.CONTINUE;
+		}
+
+		@Override
+		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+			Files.delete(dir);
+			return FileVisitResult.CONTINUE;
 		}
 	}
 }
